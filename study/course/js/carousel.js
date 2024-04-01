f4c3ca2b4de9b231c6eff731565050c56b9c1c79
Herbert Salazar <herbertsds.ufrj@gmail.com>
2024-04-01 15:35:11 -0300

    Modificando o conteúdo de estudo

--- study/course/js/carousel.js
+++ study/course/js/carousel.js
@@ -61,6 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var carouselIndex = localStorage.getItem('carouselIndex');
     if (carouselIndex) {
         $('#carouselExampleIndicators').carousel(parseInt(carouselIndex));
+        
     }
 
 });
